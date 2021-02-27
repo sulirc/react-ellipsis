80c7093b449092fe6421382085ad2fdd7bc94f75
suli <ygj2awww@gmail.com>
2021-02-27 18:30:48 +0800

    fix: response to lines change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const Text = {
   zhLong: `JavaScript (簡稱 JS) 是具有一級函數 (First-class functions) 的輕量級、直譯式或即時編譯（JIT-compiled）的程式語言。它因為用作網頁的腳本語言而大為知名，但也用於許多非瀏覽器的環境，像是 node.js、Apache CouchDB。JavaScript 是一個基於原型的 (Prototype-based)、多範型的、動態語言，支援物件導向、指令式以及宣告式 (如函數式程式設計) 風格。 閱讀關於 JavaScript 以取得更多資訊。本章節主要說明 JavaScript，不涉及網頁特有項目或主機環境。有關網頁特有的 APIs ，請參考 Web API 和 DOM。JavaScript 所採用的標準是 ECMAScript，自 2012 年起，所有現代的瀏覽器均已全面支援 ECMAScript 5.1。較老舊的瀏覽器最少也會支援 ECMAScript 3。ECMA International 於 2015 年 6 月 17 日發布第六版的 ECMAScript，其正式名稱是 ECMAScript 2015，原先被稱作 ECMAScript 6 或 ES6。從那時起， ECMAScript 標準的發布週期是一年，本文件參考了最新的草稿版本，也就是目前的 ECMAScript 2017。`,
 };
 
-const defaultLines = 5;
+const defaultLines = "l";
 const LINES = {
   s: 3,
   m: 5,
@@ -17,16 +17,17 @@ const LINES = {
 };
 
 function App() {
-  const [lines, setLines] = useState(defaultLines);
+  const [lines, setLines] = useState(LINES[defaultLines]);
   const changeVisibleLines = (e) => {
     setLines(LINES[e.target.value]);
   };
+  const handleOnElliResult = (status) => console.log(status);
 
   return (
     <>
       <label className="select-lines">
         选择显示行数：
-        <select name="select" defaultValue="m" onChange={changeVisibleLines}>
+        <select name="select" defaultValue={defaultLines} onChange={changeVisibleLines}>
           <option value="s">3</option>
           <option value="m">5</option>
           <option value="l">8</option>
@@ -35,7 +36,11 @@ function App() {
       </label>
 
       <div className="box">
-        <TextEllipsis className="ellipsis-demo" lines={lines} expand>
+        <TextEllipsis 
+          // expand
+          className="ellipsis-demo" 
+          lines={lines} 
+          onElliResult={handleOnElliResult}>
           {Text.zh}
         </TextEllipsis>
       </div>
--- src/TextEllipsis/index.jsx
+++ src/TextEllipsis/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect, useRef } from "react";
+import React, { useState, useLayoutEffect, useRef, useCallback } from "react";
 import "./index.scss";
 
 export const ELLIPSIS = {
@@ -9,19 +9,22 @@ export const ELLIPSIS = {
 export default function TextEllipsis({
   children,
   className,
-  expand,
+  expand = false,
   ellipsisChar = "...",
   ellipsisMore = "Show More",
   ellipsisLess = "Show Less",
   lines,
   lineHeight = `18px`,
+  onElliResult = () => {},
 }) {
   const [isExpand, setIsExpand] = useState(expand);
+  const [isOverflow, setIsOverflow] = useState(true);
   const ref = useRef(null);
-  const elliTextRef = useRef(null);
+  const elliLessTextRef = useRef(null);
   const elliMoreRef = useRef(null);
   const text = useRef({
     chunks: null,
+    cacheChunks: null,
     elliMoreWidth: 0,
     lastChunk: null,
   });
@@ -32,72 +35,88 @@ export default function TextEllipsis({
     lineHeight,
   };
   const maxElliHeight = parseInt(lineHeight) * lines;
-  const showMore = () => setIsExpand(true);
+  const showMore = () => {
+    reset();
+    setIsExpand(true);
+  };
   const showLess = () => setIsExpand(false);
 
-  const process = () => {
-    const container = ref.current;
-    if (container.offsetHeight > maxElliHeight) {
+  const truncate = useCallback(
+    (initialize = false) => {
+      const container = ref.current;
+      const elliText = elliLessTextRef.current;
+      const elli = text.current;
+
+      if (!elli.chunks || initialize) {
+        const chunks = elli.cacheChunks ? elli.cacheChunks.concat() : children.split(splitChar);
+
+        if (!elli.cacheChunks) {
+          elli.cacheChunks = chunks.concat();
+        }
+        elli.chunks = chunks;
+      }
+
+      if (elli.chunks.length > 1) {
+        elli.lastChunk = elli.chunks.pop();
+        elliText.innerHTML = elli.chunks.join(splitChar) + ellipsisChar;
+      } else {
+        elli.chunks = null;
+      }
+
+      if (elli.chunks && container.offsetHeight <= maxElliHeight) {
+        return;
+      }
+
       truncate();
-    }
-  };
-  const truncate = () => {
+    },
+    [maxElliHeight, children, ellipsisChar]
+  );
+
+  const process = useCallback(() => {
     const container = ref.current;
-    const elliText = elliTextRef.current;
-    const elli = text.current;
-    if (!elli.chunks) {
-      elli.chunks = children.split(splitChar);
-    }
-    if (elli.chunks.length > 1) {
-      elli.lastChunk = elli.chunks.pop();
-      elliText.innerHTML = elli.chunks.join(splitChar) + ellipsisChar;
+    console.log(lines, container.offsetHeight, maxElliHeight)
+    if (container.offsetHeight > maxElliHeight) {
+      truncate(true);
+      setIsOverflow(true);
+      onElliResult(ELLIPSIS.TRUNCATED);
     } else {
-      elli.chunks = null;
+      setIsOverflow(false);
+      onElliResult(ELLIPSIS.NOT_TRUNCATED);
     }
+  }, [maxElliHeight, truncate, onElliResult, lines]);
 
-    if (elli.chunks && container.offsetHeight <= maxElliHeight) {
-      return;
-    }
-
-    truncate();
-  };
+  const reset = useCallback(() => {
+    const elliText = elliLessTextRef.current;
+    elliText.innerHTML = children;
+  }, [children]);
 
   useLayoutEffect(() => {
     if (isExpand) {
       return;
     }
-    try {
-      text.current.elliMoreWidth = elliMoreRef.current.offsetWidth;
-    } catch (err) {
-      text.current.elliMoreWidth = 0;
-    }
+    reset();
 
+    if (elliMoreRef.current) {
+      text.current.elliMoreWidth = elliMoreRef.current.offsetWidth;
+    }
+    
     process();
-  }, [isExpand, lines]);
+  }, [children, isOverflow, isExpand, lines, process, reset]);
 
   return (
     <div ref={ref} className={`ellipsis-box ${className}`} style={ellipsisStyle}>
-      <div
-        className="truncate-text"
-        style={{
-          display: isExpand ? "none" : "block",
-        }}
-      >
-        <span ref={elliTextRef}>{children}</span>
-        <span ref={elliMoreRef} className="show-more" onClick={showMore}>
-          {ellipsisMore}
-        </span>
-      </div>
-      <div
-        className="more-text"
-        style={{
-          display: isExpand ? "block" : "none",
-        }}
-      >
-        <span>{children}</span>
-        <span className="show-less" onClick={showLess}>
-          {ellipsisLess}
-        </span>
+      <div className="truncate-text">
+        <span ref={elliLessTextRef}>{children}</span>
+        {isOverflow && !isExpand && (
+          <span ref={elliMoreRef} className="show-more" onClick={showMore}>
+            {ellipsisMore}
+          </span>
+        )}
+        {isOverflow && isExpand && (
+          <span className="show-less" onClick={showLess}>
+            {ellipsisLess}
+          </span>
+        )}
       </div>
     </div>
   );
